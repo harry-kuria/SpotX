36d65a5a81ca66a7f33553ac2d66e0bf1b9498f8
Harry-Kuria <harrisonkuria254@gmail.com>
2025-08-13 11:18:51 +0300

    🧰 CI: Setup Android SDK + Nexus Publish for Maven Central

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,11 +3,23 @@ plugins {
     id("com.android.library") version "8.5.2" apply false
     id("org.jetbrains.kotlin.android") version "1.9.24" apply false
     id("org.jetbrains.dokka") version "1.9.20" apply false
+    id("io.github.gradle-nexus.publish-plugin") version "2.0.0"
 }
 
 allprojects {
     group = "io.github.harrykuria"
     version = "0.1.0"
+}
+
+nexusPublishing {
+    repositories {
+        sonatype {
+            nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
+            snapshotRepositoryUrl.set(uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
+            username.set(providers.environmentVariable("OSSRH_USERNAME").forUseAtConfigurationTime())
+            password.set(providers.environmentVariable("OSSRH_PASSWORD").forUseAtConfigurationTime())
+        }
+    }
 }
 
 subprojects {
